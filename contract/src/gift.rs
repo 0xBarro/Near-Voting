1ec416b6c1a69ce382131293f0858feca0225c41
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-29 13:19:10 +0100

    Added the gift struc

---
+++ contract/src/gift.rs
@@ -0,0 +1,48 @@
+
+pub struct Gift {
+    url: String,
+    n_tokens_required: usize,
+    current_tokens: usize,
+}
+
+impl Gift {
+
+    // Initialized a new gift
+    pub fn new(url: &str, n_tokens_required: usize) -> Gift {
+        Gift{url: url.to_string(), n_tokens_required, current_tokens: 0}
+    } 
+
+    // Number of tokens needed for the gift
+    pub fn n_tokens_needed(&self) -> usize {self.n_tokens_required - self.current_tokens}
+
+    pub fn send_tokens(&mut self, n_tokens: usize) {
+        if n_tokens > self.n_tokens_needed() {
+            panic!("Number of tokens sent are higher than the required amount") 
+        } else {
+            self.current_tokens += n_tokens
+        }
+    }
+}
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+
+    use super::Gift;
+
+    #[test]
+    fn send_tokens() {
+        let mut g = Gift::new("some_url", 100);
+
+        assert_eq!(g.n_tokens_needed(), 100);
+        g.send_tokens(50);
+        assert_eq!(g.n_tokens_needed(), 50);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_send_tokens() {
+        let mut g = Gift::new("some_url", 100);
+        g.send_tokens(500);
+    }
+}
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,45 +1,19 @@
-
-  
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
 use near_sdk::collections::{LookupMap, LookupSet};
 
 near_sdk::setup_alloc!();
 
+mod gift;
+
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Voting {
-    pub i: usize,
+pub struct Voting { // TODO: Rename this class
+    pub accounts: LookupSet<String>,
+    pub gifts: LookupMap<String, gift::Gift>,
     pub contract_owner: String,
 }
-
-#[near_bindgen]
-impl Voting {
-    pub fn inc_value(&mut self) {
-        let log_str = format!("Signer Account Id {} Incremented the Value", env::signer_account_id());
-        self.i += 1;
-        env::log(log_str.as_bytes());
-    }
-
-    pub fn reset_value(&mut self) {
-        assert!(env::predecessor_account_id() == self.contract_owner, 
-        "Only the owner {} can call the method, not {}", self.contract_owner, env::predecessor_account_id()
-    );
-        env::log(b"Reseting Counter");
-        self.i = 0
-    }
-
-    pub fn show_value(&self) -> usize {
-        self.i
-    }
-}
-
-impl Default for Voting {
-    fn default() -> Voting {    
-        Voting {i: 0, contract_owner: env::current_account_id()}
-    }
-}   
-
 
 // use the attribute below for unit tests
 #[cfg(test)]
@@ -70,47 +44,5 @@ mod tests {
             output_data_receivers: vec![],
             epoch_height: 19,
         }
-    }
-
-    fn before_test(current_account_id: &str, signer_account_id: &str, predecessor_account_id: &str) {
-        // set up the mock context into the testing environment
-        let context = get_context(vec![],
-            false,
-            current_account_id.to_string(),
-            signer_account_id.to_string(),
-            predecessor_account_id.to_string());
-        testing_env!(context);
-    }
-
-    // mark individual unit tests with #[test] for them to be registered and fired
-    #[test]
-    fn increment() {
-
-        before_test("alice.testnet", "bob.testnet", "greg.testnet");
-        let mut contract = Voting::default();
-
-        assert_eq!(contract.i, 0);   
-        assert_eq!(contract.contract_owner, "alice.testnet");   
-        contract.inc_value();
-        assert_eq!(contract.i, 1);
-        contract.inc_value();
-        assert_eq!(contract.i, 2);
-    }
-
-    #[test]
-    #[should_panic]
-    fn reset_counter_panic() {
-        before_test("alice.testnet", "bob.testnet", "greg.testnet");
-        let mut contract = Voting::default();
-        contract.inc_value();
-        contract.reset_value();
-    }
-
-    #[test]
-    fn reset_counter() {
-        before_test("alice.testnet", "alice.testnet", "alice.testnet");
-        let mut contract = Voting::default();
-        contract.inc_value();
-        contract.reset_value();
     }
 }

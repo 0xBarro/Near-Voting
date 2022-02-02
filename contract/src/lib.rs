ceae9be5efa73f499448ba4cdf40fc27c95fb640
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-02-02 21:22:18 +0100

    Added a payable method

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -46,7 +46,9 @@ impl Voting {
 
     #[payable]
     pub fn contribute_to_gift(&mut self, account_name: &str, gift_url: &str) {
-
+        let amount_given = env::attached_deposit();
+        let donator_account = env::current_account_id();
+        println!("Thank you for giving me {}, {}", amount_given, donator_account);
     }
 
 }
@@ -61,9 +63,9 @@ mod tests {
     // part of writing unit tests is setting up a mock context
     // in this example, this is only needed for env::log in the contract
     // this is also a useful list to peek at when wondering what's available in env::*
-    fn get_context(predecessor_account_id: String, storage_usage: u64) -> VMContext {
+    fn get_context(current_account_id: String, predecessor_account_id: String, storage_usage: u64, attached_deposit: u128) -> VMContext {
         VMContext {
-            current_account_id: "alice.testnet".to_string(),
+            current_account_id,
             signer_account_id: "jane.testnet".to_string(),
             signer_account_pk: vec![0, 1, 2],
             predecessor_account_id,
@@ -73,7 +75,7 @@ mod tests {
             account_balance: 100,
             account_locked_balance: 0,
             storage_usage,
-            attached_deposit: 0,
+            attached_deposit,
             prepaid_gas: 10u64.pow(18),
             random_seed: vec![0, 1, 2],
             is_view: false,
@@ -85,7 +87,7 @@ mod tests {
     #[test]
     fn add_gift() {
         let caller_account = "alice.testnet";
-        let context = get_context(caller_account.to_string(), 0);              
+        let context = get_context(caller_account.to_string(), caller_account.to_string(), 0, 0);              
         testing_env!(context);    
 
         let mut contract = Voting::default();  
@@ -100,4 +102,25 @@ mod tests {
 
         assert_eq!(contract.gifts.get(&caller_account.to_string()).unwrap().len(), 2);
     }
+
+    #[test]
+    fn contribute_gift() {
+        // Add the gift
+        let caller_account = "alice.testnet";
+        let context = get_context(caller_account.to_string(), caller_account.to_string(), 0, 0);              
+        testing_env!(context);    
+
+        let mut contract = Voting::default();  
+        let gift1_url = "test1.com";
+
+        contract.add_gift(gift1_url, 10);
+        assert_eq!(contract.gifts.get(&caller_account.to_string()).unwrap().get(&gift1_url.to_string()).unwrap().get_url(), gift1_url);
+
+        // Pay contract
+        let context = get_context("ben.testnet".to_string(), "ben.testnet".to_string(), 0, 20);              
+        testing_env!(context);    
+        contract.contribute_to_gift(&caller_account.to_string(), gift1_url);
+
+    }
+
 }
